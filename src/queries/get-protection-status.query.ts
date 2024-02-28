152088660355266e5897551fe9975f656cb9704c
bytdnel <danielbyta.work@gmail.com>
2024-02-28 16:07:56 +0300

    a few changes

--- src/queries/get-protection-status.query.ts
+++ src/queries/get-protection-status.query.ts
@@ -8,20 +8,16 @@ export enum ProtectionStatus {
 }
 
 export class GetProtectionStatusQuery {
-  // private readonly winRegistryCommands = {
-  //   valueExists: promisify(this.winRegistry.valueExists),
-  //   set: promisify(this.winRegistry.set),
-  //   get: promisify(this.winRegistry.get),
-  //   values: promisify(this.winRegistry.values),
-  // };
+  private readonly mapper = {
+    0: ProtectionStatus.Disabled,
+    1: ProtectionStatus.Enabled,
+  };
 
   public async execute(): Promise<ProtectionStatus> {
-    const protectionStatus = await this.getProtectionStatus();
-
-    return this.mapProtectionStatus(protectionStatus);
+    return this.getProtectionStatus();
   }
 
-  private async getProtectionStatus(): Promise<number> {
+  private async getProtectionStatus(): Promise<ProtectionStatus> {
     const isWindows = os.platform() === "win32";
 
     console.log("GetProtectionStatusQuery", {
@@ -34,93 +30,25 @@ export class GetProtectionStatusQuery {
       : this.getMockedProtectionStatus();
   }
 
-  private async getDeviceProtectionTools(): Promise<number> {
+  private async getProtectionStatusFromWinReg(): Promise<ProtectionStatus> {
+    const hasAccess = await this.hasRegAccesss();
+    return hasAccess ? ProtectionStatus.Disabled : ProtectionStatus.Enabled;
+  }
+
+  private getMockedProtectionStatus(): ProtectionStatus {
+    const randomStatus = Math.round(Math.random()) as 0 | 1;
+
+    return this.mapper[randomStatus];
+  }
+
+  private async hasRegAccesss(): Promise<boolean> {
     const registry = new Registry({
       hive: Registry.HKCU,
       key: "\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System",
     });
 
-    await new Promise<void>((resolve, reject) => {
-      registry.values((err, items) => {
-        console.log({
-          err,
-          items,
-          values: items?.map((i) => [i.name, i.key, i.value]),
-        });
-
-        if (err) return reject(err);
-        resolve();
-      });
-    });
-
-    const disableRegistryTools = "DisableRegistryTools";
-
-    const exists = await new Promise<boolean>((resolve) => {
-      registry.get(disableRegistryTools, (error, item) => {
-        console.log({
-          error,
-          item,
-          value: item?.value,
-        });
-
-        resolve(error || !item?.value ? false : true);
-      });
-    });
-
-    console.log({ exists });
-
-    if (!exists) {
-      await new Promise<void>((resolve, reject) => {
-        registry.set(
-          disableRegistryTools,
-          Registry.REG_DWORD,
-          "0x0",
-          (setWinErgValueError) => {
-            if (setWinErgValueError) {
-              console.log({ setWinErgValueError });
-              return reject(setWinErgValueError);
-            }
-
-            resolve();
-          }
-        );
-      });
-    }
-
-    return new Promise<number>((resolve, reject) => {
-      registry.get(disableRegistryTools, (error, item) => {
-        console.log({
-          error,
-          item,
-          value: item.value,
-          intValue: parseInt(item.value),
-        });
-
-        resolve(parseInt(item.value));
-      });
-    });
-  }
-
-  private async getProtectionStatusFromWinReg(): Promise<number> {
-    return this.getDeviceProtectionTools();
-  }
-
-  private getMockedProtectionStatus(): number {
-    return Math.round(Math.random());
-  }
-
-  private mapProtectionStatus(status: number): ProtectionStatus {
-    const mapper: Record<number, ProtectionStatus> = {
-      0: ProtectionStatus.Disabled,
-      1: ProtectionStatus.Enabled,
-    };
-
-    const mappedStatus = mapper[status];
-
-    if (!mappedStatus) {
-      throw new Error("E_PROTECTION_STATUS_INVALID");
-    }
-
-    return mappedStatus;
+    return await promisify(registry.values)()
+      .then(() => true)
+      .catch(() => false);
   }
 }
