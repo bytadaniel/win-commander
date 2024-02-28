8fb7b1d9f44541467cf9790cd3e795ab83b900f0
bytdnel <danielbyta.work@gmail.com>
2024-02-28 17:15:16 +0300

    make executables

---
+++ make/osx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run node with experimental sea-config
+node --experimental-sea-config sea-config.json
+
+# Copy node executable to pc-commander
+cp "$(command -v node)" pc-commander
+
+# Remove signature from pc-commander
+codesign --remove-signature pc-commander
+
+# Run postject to generate blob
+npx postject pc-commander NODE_SEA_BLOB pc-commander.blob \
+    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
+    --macho-segment-name NODE_SEA
+
+# Sign -commander
+codesign --sign - pc-commander
+
+rm pc-commander.blob
