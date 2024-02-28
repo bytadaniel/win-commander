a3c4fc97e594a452939e8e2603be3aacda757eef
bytdnel <danielbyta.work@gmail.com>
2024-02-28 12:51:35 +0300

    return mac addresss to upper case

--- src/queries/get-mac-address.query.ts
+++ src/queries/get-mac-address.query.ts
@@ -2,7 +2,7 @@ import getMac, { isMAC } from "getmac";
 
 export class GetMacAddressQuery {
   public execute(): string {
-    const macAddress = getMac();
+    const macAddress = getMac().toUpperCase();
 
     if (!isMAC(macAddress)) {
       throw new Error("E_MAC_INVALID");
