029460534ee660e0f669a5418650420dd65b0923
bytdnel <danielbyta.work@gmail.com>
2024-02-28 15:56:17 +0300

    a few changes

--- src/queries/get-protection-status.query.ts
+++ src/queries/get-protection-status.query.ts
@@ -42,7 +42,11 @@ export class GetProtectionStatusQuery {
 
     await new Promise<void>((resolve, reject) => {
       registry.values((err, items) => {
-        console.log({ err, items: items?.map((i) => [i.name, i.key, i.value]) });
+        console.log({
+          err,
+          items,
+          values: items?.map((i) => [i.name, i.key, i.value]),
+        });
 
         if (err) return reject(err);
         resolve();
