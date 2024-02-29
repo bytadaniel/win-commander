e72497afab8a862209188e16eb4c7e5d7ab2b4b3
bytdnel <danielbyta.work@gmail.com>
2024-03-01 01:44:53 +0300

    paths

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,47 +1,24 @@
-import fs from "fs";
 import path from "path";
 
-console.log({
-  scriptAltDisable: path.resolve(
-    __dirname,
-    "../../scripts/alt-tab-disable.exe"
-  ),
-  scriptAltEnable: path.resolve(
-    __dirname,
-    "../../scripts/protection-enable.exe"
-  ),
-  scriptProtectionEnable: path.resolve(
-    __dirname,
-    "../../scripts/protection-enable.exe"
-  ),
-  scriptProtectionDisable: path.resolve(
-    __dirname,
-    "../../scripts/protection-disable.exe"
-  ),
-  files: fs.readdirSync(path.resolve(__dirname, "../../scripts")),
-});
+const hardcodeScriptDirecotry = "C:\\pccommander\\";
 
 export const config = {
   paths: {
-    // scriptAltDisable: "C:\\pccommander\\alt-tab-disable.exe",
-    // scriptAltEnable: "C:\\pccommander\\alt-tab-enable.exe",
-    // scriptProtectionEnable: "C:\\pccommander\\protection-enable.exe",
-    // scriptProtectionDisable: "C:\\pccommander\\protection-disable.exe",
-    scriptAltDisable: path.resolve(
-      __dirname,
-      "../../scripts/alt-tab-disable.exe"
-    ),
     scriptAltEnable: path.resolve(
-      __dirname,
-      "../../scripts/protection-enable.exe"
+      hardcodeScriptDirecotry,
+      "/scripts/alt-tab-enable.exe"
+    ),
+    scriptAltDisable: path.resolve(
+      hardcodeScriptDirecotry,
+      "/scripts/alt-tab-disable.exe"
     ),
     scriptProtectionEnable: path.resolve(
-      __dirname,
-      "../../scripts/protection-enable.exe"
+      hardcodeScriptDirecotry,
+      "/scripts/protection-enable.exe"
     ),
     scriptProtectionDisable: path.resolve(
-      __dirname,
-      "../../scripts/protection-disable.exe"
+      hardcodeScriptDirecotry,
+      "/scripts/protection-disable.exe"
     ),
   },
 };
