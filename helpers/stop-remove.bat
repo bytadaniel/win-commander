8f0db3b404104a5feba15495ff9063e459c6d00a
bytdnel <danielbyta.work@gmail.com>
2024-03-01 02:05:02 +0300

    stop-remove.bat

---
+++ helpers/stop-remove.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+:: Путь к исполняемому файлу NSSM
+set NSSM_PATH=.\helpers\nssm.exe
+
+:: Имя службы, которую вы хотите удалить
+set SERVICE_NAME=PcCommanderService
+
+:: Удаление службы
+"%NSSM_PATH%" stop %SERVICE_NAME%
+"%NSSM_PATH%" remove %SERVICE_NAME%
+if %ERRORLEVEL% neq 0 (
+    echo Failed to remove service %SERVICE_NAME%.
+    exit /b %ERRORLEVEL%
+)
+
+echo Service %SERVICE_NAME% removed successfully.
+
