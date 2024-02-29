3f8d0109ec3da5b3760a5c1f12ab80f4ef82716b
bytdnel <danielbyta.work@gmail.com>
2024-03-01 02:13:44 +0300

    paths

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,24 +1,8 @@
-import path from "path";
-
-const hardcodeScriptDirecotry = "C:\\pccommander\\";
-
 export const config = {
   paths: {
-    scriptAltEnable: path.resolve(
-      hardcodeScriptDirecotry,
-      "./scripts/alt-tab-enable.exe"
-    ),
-    scriptAltDisable: path.resolve(
-      hardcodeScriptDirecotry,
-      "./scripts/alt-tab-disable.exe"
-    ),
-    scriptProtectionEnable: path.resolve(
-      hardcodeScriptDirecotry,
-      "./scripts/protection-enable.exe"
-    ),
-    scriptProtectionDisable: path.resolve(
-      hardcodeScriptDirecotry,
-      "./scripts/protection-disable.exe"
-    ),
+    scriptAltDisable: "C:\\pccommander\\scripts\\alt-tab-disable.exe",
+    scriptAltEnable: "C:\\pccommander\\scripts\\alt-tab-enable.exe",
+    scriptProtectionEnable: "C:\\pccommander\\scripts\\protection-enable.exe",
+    scriptProtectionDisable: "C:\\pccommander\\scripts\\protection-disable.exe",
   },
 };
