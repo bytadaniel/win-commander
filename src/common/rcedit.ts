32ff8d8aa3ff3a54200f55489bed701294e1a975
bytdnel <danielbyta.work@gmail.com>
2024-03-01 03:02:50 +0300

    rcedit

--- src/common/rcedit.ts
+++ src/common/rcedit.ts
@@ -2,17 +2,17 @@ import rcedit from "rcedit";
 import { config } from "./config";
 
 (async () => {
-  await rcedit(config.paths.pcCommander, {
-    "version-string": {
-      ProductName: "PcCommanderService"
-    },
-  });
+  // await rcedit(config.paths.pcCommander, {
+  //   "version-string": {
+  //     ProductName: "PcCommanderService"
+  //   },
+  // });
 
-  await rcedit(config.paths.pcCommander, {
-    "version-string": {
-      Comments: "PcCommanderService"
-    },
-  });
+  // await rcedit(config.paths.pcCommander, {
+  //   "version-string": {
+  //     Comments: "PcCommanderService"
+  //   },
+  // });
 
   await rcedit(config.paths.pcCommander, {
     "version-string": {
