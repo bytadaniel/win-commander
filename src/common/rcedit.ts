add6d82622b38b8455a9c2283813e601422bd503
bytdnel <danielbyta.work@gmail.com>
2024-03-01 03:00:18 +0300

    rcedit

--- src/common/rcedit.ts
+++ src/common/rcedit.ts
@@ -1,11 +1,10 @@
 import rcedit from "rcedit";
 import { config } from "./config";
 
-rcedit(config.paths.pcCommander, {
-  "version-string": {
-    ProductName: "PcCommanderService",
-    OriginalFilename: "pc-commander.exe",
-    CompanyName: "Tehopolis",
-    Comments: "PcCommanderService by Tehnopolis",
-  },
-});
+(async () => {
+  await rcedit(config.paths.pcCommander, {
+    "version-string": {
+      ProductName: "PcCommanderService"
+    },
+  });
+})()
